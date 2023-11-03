f3d438d12d8c95ac951437f677ab871f56ca7dff
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 18:38:04 +0100

    custom guidelines: always/never formulations

--- cli/src/utils/prompting/custom_guidelines.ts
+++ cli/src/utils/prompting/custom_guidelines.ts
@@ -1,6 +1,6 @@
 export const customGuidelines = [
-  `Use strictest TypeScript settings in the code you generate.`,
+  `Always use strictest TypeScript settings in the code you generate.`,
   // `Generate unit tests`,
   // `Use newest libraries, API-s and language settings and style.`
-  `Do not add explanatory comments, unless something really unusual is going on.`, 
+  `Never add explanatory comments, unless something really unusual is going on.`, 
 ];
