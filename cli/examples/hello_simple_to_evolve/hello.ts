0425eefee227c433991cf06bb0ae86e101a7a794
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 21:03:55 +0100

    split blueBold

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -1,14 +1,15 @@
 // start
 const red = '\x1b[31m%s\x1b[0m';
-const blueBold = '\x1b[34m%s\x1b[1m';
+const blue = '\x1b[34m%s\x1b[0m';
+const bold = '\x1b[1m%s\x1b[0m';
 const reset = '\x1b[0m';
 
 for(let i = 0; i < 3; i++) {
   if(i % 2 == 0) console.log(red, 'hello earth') // red
   if(i == 2) {
     console.debug('bye')
-    console.log(blueBold, 'goodbye\x1b[0m') // blue, bold
-    console.log(blueBold, 'goodbye\x1b[0m') // blue, bold
+    console.log(blue, bold, 'goodbye\x1b[0m') // blue, bold
+    console.log(blue, bold, 'goodbye\x1b[0m') // blue, bold
   }
 console.log(reset) // reset color
 }
