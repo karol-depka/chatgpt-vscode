5736b26b6a3fe371588b8b4d9ae625d704bdd6dd
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 21:38:31 +0100

    prompt: ... purple welcome

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -1,12 +1,14 @@
 // start
 const red = '\x1b[31m%s\x1b[0m';
 const blue = '\x1b[34m%s\x1b[0m';
+const purple = '\x1b[35m%s\x1b[0m';
 const bold = '\x1b[1m%s\x1b[0m';
 const reset = '\x1b[0m';
 import fs from 'fs';
 const logStream = fs.createWriteStream('log.txt', {flags: 'a'});
 
 for(let i = 0; i < 3; i++) {
+  console.log(purple, 'welcome') // purple
   if(i % 2 == 0) console.log(red, 'hello earth') // red
     logStream.write('hello earth\n');
   if(i == 2) {
