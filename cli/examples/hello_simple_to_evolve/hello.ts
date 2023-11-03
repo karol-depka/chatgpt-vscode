40dcc03c4e14211a3402d4948304d4ba14d03185
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 20:51:36 +0100

    prompt: "print goodbye at the end, in blue color, in bold; make sure to reset color at the end, for those who use terminal afterwards;"

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -1,5 +1,5 @@
 // start
 console.log('\x1b[31m%s\x1b[0m', 'hello world') // red
 console.debug('bye')
-console.log('\x1b[33m%s\x1b[1m', 'goodbye') // orange, bold
+console.log('\x1b[34m%s\x1b[1m', 'goodbye\x1b[0m') // blue, bold
 // end
