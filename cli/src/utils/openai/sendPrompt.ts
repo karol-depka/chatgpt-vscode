e0f6fd5d44674a60b50958c4869943488b94717b
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 22:46:25 +0100

    chalk colors

--- cli/src/utils/openai/sendPrompt.ts
+++ cli/src/utils/openai/sendPrompt.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import OpenAI from "openai";
 import { makePrompt } from "../prompting/makePrompt";
+import { green, reset } from "chalk";
 import { MPFullPrompt, MPUserPrompt, MPPatchContent } from "../types";
 import { MPPromptInputs } from "../prompting/types";
-import { green } from "../colors";
+import chalk from 'chalk'
 import { extractCodeFromMarkdown } from "../markdown/markdown_utils";
 dotenv.config();
 
@@ -16,9 +17,9 @@ export async function sendFullPrompt(fullPromptTextToSend: MPFullPrompt) {
   console.log("initializing OpenAI");
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-  });
+  }); console.log(chalk.green("initializing OpenAI"));
 
-  console.log("sendFullPrompt:");
+  console.log(chalk.inverse("sendFullPrompt:"));
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: fullPromptTextToSend }],
     // model: "gpt-3.5-turbo",
@@ -26,11 +27,12 @@ export async function sendFullPrompt(fullPromptTextToSend: MPFullPrompt) {
     // temperature: 0,
     top_p: 0.1,
   });
+  console.log(chalk.inverse("END sendFullPrompt"));
 
   // console.debug(`chatCompletion.choices`, chatCompletion.choices);
   const responseContent = chatCompletion.choices[0].message.content;
   // console.debug(`chatCompletion.choices...`, responseContent);
-  console.log(green + `responseContent:${responseContent}` + "\x1b[0m");
+  console.log(green(`responseContent:${responseContent}`) + reset());
   const responsePatch = extractCodeFromMarkdown(
     responseContent!
   ) as MPPatchContent;
