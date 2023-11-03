02175f4cee33a5b92c6088e64580ddb61400ba68
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 20:49:48 +0100

    hello.ts prompted modifications

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -1,5 +1,5 @@
 // start
 console.log('\x1b[31m%s\x1b[0m', 'hello world') // red
 console.debug('bye')
-console.log('\x1b[33m%s\x1b[0m', 'goodbye') // brown
+console.log('\x1b[33m%s\x1b[1m', 'goodbye') // orange, bold
 // end
