319e3b65fb6e8bf80176bcb71c77fcfbbbe12d08
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 21:08:35 +0100

    t

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -3,13 +3,18 @@ const red = '\x1b[31m%s\x1b[0m';
 const blue = '\x1b[34m%s\x1b[0m';
 const bold = '\x1b[1m%s\x1b[0m';
 const reset = '\x1b[0m';
+const fs = require('fs');
+const logStream = fs.createWriteStream('log.txt', {flags: 'a'});
 
 for(let i = 0; i < 3; i++) {
   if(i % 2 == 0) console.log(red, 'hello earth') // red
+    logStream.write('hello earth\n');
   if(i == 2) {
     console.debug('bye')
+    logStream.write('bye\n');
     console.log(blue + bold + 'goodbye\x1b[0m') // blue, bold
     console.log(blue + bold + 'goodbye\x1b[0m') // blue, bold
+    logStream.write('goodbye\n');
   }
   console.log(reset + '') // reset color
 }
