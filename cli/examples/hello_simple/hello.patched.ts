8e63cd7b7c510f0b6c55da84c9235661901e3045
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 13:10:04 +0100

    blue goodnight

--- cli/examples/hello_simple/hello.patched.ts
+++ cli/examples/hello_simple/hello.patched.ts
@@ -1,9 +1,3 @@
-for(let i = 0; i < 7; i++) {
-  console.log('\x1b[31m', 'hello world', '\x1b[0m');
-  console.debug('\x1b[33m', 'bye', '\x1b[0m');
-}
-console.log('\x1b[34m\x1b[1m', 'Goodnight!', '\x1b[0m');
-
 // start
 console.log('hello world')
 console.debug('bye')
