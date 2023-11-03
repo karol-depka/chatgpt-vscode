db1a07318c08bc628696d8bbbd84a4ed671d21b6
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 20:54:40 +0100

    prompt: ""extract the terminal commands as const-s""

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -1,7 +1,11 @@
 // start
-console.log('\x1b[31m%s\x1b[0m', 'hello world') // red
+const redHello = '\x1b[31m%s\x1b[0m';
+const blueBoldGoodbye = '\x1b[34m%s\x1b[1m';
+const resetColor = '\x1b[0m';
+
+console.log(redHello, 'hello world') // red
 console.debug('bye')
-console.log('\x1b[34m%s\x1b[1m', 'goodbye\x1b[0m') // blue, bold
-console.log('\x1b[34m%s\x1b[1m', 'goodbye\x1b[0m') // blue, bold
-console.log('\x1b[0m') // reset color
+console.log(blueBoldGoodbye, 'goodbye\x1b[0m') // blue, bold
+console.log(blueBoldGoodbye, 'goodbye\x1b[0m') // blue, bold
+console.log(resetColor) // reset color
 // end
