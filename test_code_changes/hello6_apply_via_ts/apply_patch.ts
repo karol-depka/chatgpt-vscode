f487bb09e5cc761680facb2d28c7f283fedff024
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 10:03:59 +0100

    Add logging to apply_patch.ts

--- test_code_changes/hello6_apply_via_ts/apply_patch.ts
+++ test_code_changes/hello6_apply_via_ts/apply_patch.ts
@@ -10,6 +10,7 @@ interface Chunk {
 }
 
 function parsePatch(content: string): Patch {
+    console.log('Parsing patch...');
     const lines = content.split('\n');
     const patch: Patch = { header: [], chunks: [] };
     let chunk: Chunk | null = null;
@@ -31,10 +32,12 @@ function parsePatch(content: string): Patch {
         patch.chunks.push(chunk);
     }
 
+    console.log('Finished parsing patch.');
     return patch;
 }
 
 function applyPatch(original: string, patch: Patch): string {
+    console.log('Applying patch...');
     const originalLines = original.split('\n');
     let output: string[] = [];
     let currentIndex = 0;
@@ -59,6 +62,7 @@ function applyPatch(original: string, patch: Patch): string {
 
         if (!foundContext) {
             // If no context was found, just skip this chunk
+            console.log('No context found for chunk, skipping...');
             continue;
         }
 
@@ -66,12 +70,15 @@ function applyPatch(original: string, patch: Patch): string {
         for (let line of chunk.lines) {
             if (line.startsWith('-')) {
                 // Remove line
+                console.log(`Removing line: ${line}`);
                 currentIndex++;
             } else if (line.startsWith('+')) {
                 // Add line
+                console.log(`Adding line: ${line}`);
                 output.push(line.slice(1));
             } else {
                 // Context line, keep from original
+                console.log(`Keeping original line: ${line}`);
                 output.push(originalLines[currentIndex]);
                 currentIndex++;
             }
@@ -84,11 +91,13 @@ function applyPatch(original: string, patch: Patch): string {
         currentIndex++;
     }
 
+    console.log('Finished applying patch.');
     return output.join('\n');
 }
 
 // Main function
 function patchFile(filePath: string, patchPath: string): void {
+    console.log(`Patching file ${filePath} with patch ${patchPath}...`);
     const originalContent = fs.readFileSync(filePath, 'utf-8');
     const patchContent = fs.readFileSync(patchPath, 'utf-8');
 
@@ -96,8 +105,10 @@ function patchFile(filePath: string, patchPath: string): void {
     const result = applyPatch(originalContent, patch);
 
     fs.writeFileSync(filePath + ".applied.ts", result);
+    console.log(`Finished patching file. Output written to ${filePath}.applied.ts`);
 }
 
 // Example usage
 
-patchFile('hello.orig.ts', 'hello.patch');
+// patchFile('../../cli/src/index.ts', 'hello.patch');
+patchFile('apply_patch.ts', 'hello.patch');
