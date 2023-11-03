9271203030463543edf4cf7ac18e9e6d75ed7cf8
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 22:35:01 +0100

    semi-bad patch

--- cli/examples/hello_simple_to_evolve/colorGen.ts
+++ cli/examples/hello_simple_to_evolve/colorGen.ts
@@ -1,6 +1,18 @@
 // put code here
 
+function generateGradient(): void {
+  for (let i = 0; i <= 255; i++) {
+    let red = i;
+    let green = 0;
+    let blue = 0;
+    let colorString = `\x1b[38;2;${red};${green};${blue}m\u2588\u2588\x1b[0m`;
+    console.log(colorString);
+  }
+}
+
 console.log('start')
+
+generateGradient();
 
 
 console.log('finish')
