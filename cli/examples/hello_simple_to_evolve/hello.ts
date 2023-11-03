579155f1e369336eff24e1e2fb28b087bc63146c
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 20:58:20 +0100

    loop

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -3,9 +3,11 @@ const red = '\x1b[31m%s\x1b[0m';
 const blueBold = '\x1b[34m%s\x1b[1m';
 const reset = '\x1b[0m';
 
+for(let i = 0; i < 3; i++) {
 console.log(red, 'hello world') // red
 console.debug('bye')
 console.log(blueBold, 'goodbye\x1b[0m') // blue, bold
 console.log(blueBold, 'goodbye\x1b[0m') // blue, bold
 console.log(reset) // reset color
+}
 // end
