e78899039bcd1128f927d42d9c5ff79b14ff4a7b
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-04 00:04:51 +0100

    makeAndSendFullPrompt and disable chalk

--- cli/src/utils/openai/sendPrompt.ts
+++ cli/src/utils/openai/sendPrompt.ts
@@ -1,22 +1,29 @@
 import dotenv from "dotenv";
 import OpenAI from "openai";
 import { makePrompt } from "../prompting/makePrompt";
-import { MPFullPrompt, MPUserPrompt, MPPatchContent } from "../types";
+import { MPFullPrompt, MPPatchContent } from "../types";
 import { MPPromptInputs } from "../prompting/types";
-import chalk from 'chalk'
+// import chalk from 'chalk'
 import { extractCodeFromMarkdown } from "../markdown/markdown_utils";
+import {main} from "./sendPromptStreaming";
 dotenv.config();
+const idFun = <T>(x: T) => x;
+const chalk = {
+  green: idFun,
+  inverse: idFun,
+}
 
-export function makeAndSendFullPrompt(promptInputs: MPPromptInputs) {
+export async function makeAndSendFullPrompt(promptInputs: MPPromptInputs) {
   const fullPromptTextToSend = makePrompt(promptInputs);
+  // main();
   return sendFullPrompt(fullPromptTextToSend);
 }
 
 export async function sendFullPrompt(fullPromptTextToSend: MPFullPrompt) {
-  console.log("initializing OpenAI");
+  console.log(chalk.green("initializing OpenAI"));
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-  }); console.log(chalk.green("initializing OpenAI"));
+  });
 
   console.log(chalk.inverse("sendFullPrompt:"));
   const chatCompletion = await openai.chat.completions.create({
@@ -25,13 +32,14 @@ export async function sendFullPrompt(fullPromptTextToSend: MPFullPrompt) {
     model: "gpt-4",
     // temperature: 0,
     top_p: 0.1,
+    // stream: true,
   });
-  console.log(chalk.inverse("END sendFullPrompt"));
+  // console.log(chalk.inverse("END sendFullPrompt"));
 
   // console.debug(`chatCompletion.choices`, chatCompletion.choices);
   const responseContent = chatCompletion.choices[0].message.content;
   // console.debug(`chatCompletion.choices...`, responseContent);
-  console.log(green(`responseContent:${responseContent}`) + reset());
+  // console.log(chalk.green(`responseContent:${responseContent}`));
   const responsePatch = extractCodeFromMarkdown(
     responseContent!
   ) as MPPatchContent;
