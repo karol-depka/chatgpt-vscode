e47e8ac453e2c1463128626bc05411197d93edfa
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-03 22:05:21 +0100

    HELLO ALVARO and PARKER' in orange, right after iteration number; introduce const for orange

--- cli/examples/hello_simple_to_evolve/hello.ts
+++ cli/examples/hello_simple_to_evolve/hello.ts
@@ -3,6 +3,7 @@ const red = '\x1b[31m%s\x1b[0m';
 const blue = '\x1b[34m%s\x1b[0m';
 const purple = '\x1b[35m%s\x1b[0m';
 const yellow = '\x1b[33m%s\x1b[0m';
+const orange = '\x1b[33m%s\x1b[0m';
 const bold = '\x1b[1m%s\x1b[0m';
 const reset = '\x1b[0m';
 import fs from 'fs';
@@ -10,9 +11,9 @@ const logStream = fs.createWriteStream('log.txt', {flags: 'a'});
 
 for(let i = 0; i < 3; i++) {
   console.log(yellow, i); // yellow
+  console.log(orange, 'HELLO ALVARO and PARKER'); // orange
   console.log(purple, 'welcome') // purple
   console.log(purple, 'welcome') // purple
-  console.log('\x1b[33m%s\x1b[0m', 'HELLO ALVARO and PARKER'); // orange
   if(i % 2 != 0) console.log(purple, 'this is odd!') // violet
   if(i % 2 == 0) console.log(red, 'hello earth') // red
     logStream.write('hello earth\n');
