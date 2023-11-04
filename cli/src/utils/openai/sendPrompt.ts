49c69e46f598030e9588f7970d58a8d3145928ea
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-04 02:16:11 +0100

    streaming prototype

--- cli/src/utils/openai/sendPrompt.ts
+++ cli/src/utils/openai/sendPrompt.ts
@@ -7,7 +7,11 @@ import { MPPromptInputs } from "../prompting/types";
 import chalk from 'chalk'
 import { extractCodeFromMarkdown } from "../markdown/markdown_utils";
 import {main} from "./sendPromptStreaming";
-import {ChatCompletionCreateParams, ChatCompletionCreateParamsNonStreaming} from "openai/resources";
+import {
+  ChatCompletionCreateParams,
+  ChatCompletionCreateParamsNonStreaming,
+  ChatCompletionCreateParamsStreaming
+} from "openai/resources";
 dotenv.config();
 
 export async function makeAndSendFullPrompt(promptInputs: MPPromptInputs) {
@@ -28,28 +32,35 @@ export async function sendFullPrompt(fullPromptTextToSend: MPFullLLMPrompt) {
   // const model = "gpt-3.5-turbo";
   const model = "gpt-4";
   console.log('Using model ' + chalk.blue(model));
-  const body: ChatCompletionCreateParamsNonStreaming = {
+  // const body: ChatCompletionCreateParamsNonStreaming = {
+  const body: ChatCompletionCreateParamsStreaming = {
     messages: [{ role: "user", content: fullPromptTextToSend }],
     model: model,
     // temperature: 0,
     top_p: 0.1,
-    // stream: true,
+    stream: true,
   };
   console.log(chalk.green("calling openai.chat.completions.create body"), body);
-  const chatCompletion = await openai.chat.completions.create(body);
+  const completion = await openai.chat.completions.create(body);
   // console.log(chalk.inverse("END sendFullPrompt"));
   const t1 = performance.now();
   const timeTaken = ((t1 - t0) / 1000).toFixed(1);
   console.log(`API request took ${timeTaken} seconds`);
 
+  for await (const chunk of completion) {
+    console.log('chunk', chunk.choices[0].delta.content);
+  }
+
   // console.debug(`chatCompletion.choices`, chatCompletion.choices);
-  const responseContent = chatCompletion.choices[0].message.content;
+  // const responseContent = completion.choices[0].message.content;
   // console.debug(`chatCompletion.choices...`, responseContent);
   // console.log(chalk.green(`responseContent:${responseContent}`));
-  const responsePatch = extractCodeFromMarkdown(
-    responseContent!
-  ) as MPPatchContent;
-  return { chatCompletion, responsePatch, printCostsFunc: () => {
+
+  const responsePatch = '' as MPPatchContent;
+  // const responsePatch = extractCodeFromMarkdown(
+  //   responseContent!
+  // ) as MPPatchContent;
+  return { chatCompletion: completion, responsePatch, printCostsFunc: () => {
     // FIXME
   } };
 }
