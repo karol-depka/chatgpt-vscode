89a8b6bd8cd2d4478a4ec80444329b7022cc3c0f
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-05 19:22:31 +0100

    meta-llama/codellama-34b-instruct"; // produced multiple-files - multiple diffs, presidents

--- cli/examples/empty.ts
+++ cli/examples/empty.ts
@@ -1,3 +1,11 @@
 // program starting
 
+// import the presidents data
+import { presidents } from './presidents.ts';
+
+// print the 10 most recent presidents
+console.log('The 10 most recent presidents are:');
+for (let i = 0; i < 10; i++) {
+  console.log(presidents[i].name);
+}
 // program ending
--- cli/src/utils/openai/model.ts
+++ cli/src/utils/openai/model.ts
@@ -1,5 +1,5 @@
 // export const model = "gpt-4";
-export const model = "google/palm-2-codechat-bison-32k";
+// export const model = "google/palm-2-codechat-bison-32k";
 // export const model = "anthropic/claude-2ZZZ"; // works interestingly;
-// export const model = "meta-llama/codellama-34b-instruct";
+export const model = "meta-llama/codellama-34b-instruct"; // produced multiple-files - multiple diffs, presidents
 // export const model = "gpt-3.5-turbo";
