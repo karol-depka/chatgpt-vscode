ca146c84b7446350339dc45d9dec75b6f9500cbe
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-05 06:33:13 +0100

    Executing via execSync()

--- cli/src/utils/exec/execute_file.ts
+++ cli/src/utils/exec/execute_file.ts
@@ -1,9 +1,15 @@
 import { MPFilePath } from "../types";
 import { coloredFilePath } from "../fs/fsUtils";
+import { execSync } from "child_process";
 
 export function executeFile(patchedFilePath: MPFilePath) {
-  require("ts-node").register();
+  //   require("ts-node").register();
   console.log("===== Will execute", coloredFilePath(patchedFilePath));
-  require("../" + patchedFilePath);
+  //   require("../" + patchedFilePath);
+  try {
+    execSync(`ts-node ${patchedFilePath}`, { stdio: "inherit" });
+  } catch (error) {
+    console.error("Error executing file:", error);
+  }
   console.log("===== Finished executing", coloredFilePath(patchedFilePath));
 }
