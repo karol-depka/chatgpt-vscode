e5fa57c74d843842a0ba34456a303a09fdff9fe3
Karol Depka <karol-depka@users.noreply.github.com>
2023-11-05 06:39:27 +0100

    export const all models

--- cli/src/utils/openai/model.ts
+++ cli/src/utils/openai/model.ts
@@ -1,4 +1,4 @@
-// const model = "gpt-4";
-// const model = "anthropic/claude-2ZZZ"; // works interestingly;
-// const model = "meta-llama/codellama-34b-instruct";
-export const model = "gpt-3.5-turbo";
+export const model = "gpt-4";
+// export const model = "anthropic/claude-2ZZZ"; // works interestingly;
+// export const model = "meta-llama/codellama-34b-instruct";
+// export const model = "gpt-3.5-turbo";
